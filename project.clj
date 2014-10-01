efd55292f4f6ca46603a64a32962ff27481e576b
Alistair Roche <roche.a@gmail.com>
2014-10-02 09:25:11 +1000

    Clean up readme and project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject req-gen "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Zendesk App Creator"
+  :url "https://github.com/atroche/zendesk_app_creator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
